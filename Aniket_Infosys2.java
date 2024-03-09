28d77cb3e6383d2f34db572cb77ce10696e9b211
Aniket Practice <waghmareaniket150@gmail.com>
2024-03-09 21:06:50 +0530

    Edited

---
+++ Aniket_Infosys2.java
@@ -0,0 +1,8 @@
+package Infosys;
+
+public class Aniket_Infosys2 {
+    public static void main(String[] args) {
+        System.out.println("QA Manager at INFOSYS");
+        System.out.println("Soon will be switch to Adobe");
+    }
+}
